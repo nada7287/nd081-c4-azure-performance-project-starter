2c5e10fd1dd9d7aea9cb970135ce455271158151
nada7287 <najahdawahi@gmail.com>
2021-06-13 17:11:48 +0200

    first commit

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -76,11 +76,11 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
-        tracer.span(name="Cats Vote")
+        tracer.span(name="Cats voted")
 
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-        tracer.span(name="Dogs Vote")
+        tracer.span(name="Dogs voted ")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
