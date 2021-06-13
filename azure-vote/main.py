c23542d94e659bf009684761e694318e28110084
nada7287 <najahdawahi@gmail.com>
2021-06-13 20:21:44 +0200

    commit instrumentaion key change

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -37,8 +37,9 @@ exporter = metrics_exporter.new_metrics_exporter(enable_standard_metrics=True,co
 #tracer = # TODO: Setup tracer
 
 tracer = Tracer(exporter=AzureExporter(connection_string="InstrumentationKey=dc5bfc54-1492-408f-870d-ed6142af3a70"),sampler=ProbabilitySampler(1.0))
-app = Flask(__name__)
+tc = TelemetryClient('dc5bfc54-1492-408f-870d-ed6142af3a70')
 
+app = Flask(__name__)
 # Requests
 #middleware = # TODO: Setup flask middleware
 middleware = FlaskMiddleware(app,exporter=AzureExporter(connection_string='InstrumentationKey=dc5bfc54-1492-408f-870d-ed6142af3a70'),sampler=ProbabilitySampler(rate=1.0),)
@@ -80,11 +81,13 @@ def index():
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
         tracer.span(name="Cats")
-
+        tc.track_event("Cats")
+        tc.flush()
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
         tracer.span(name="Dogs")
-
+        tc.track_event("Cats")
+        tc.flush()
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
